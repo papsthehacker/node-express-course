25b1ecdf7723372ef544dff57cc54310ef324d89
Promise <papsthehacker@gmail.com>
2021-07-07 02:35:17 +0000

    add first GET route

--- server.js
+++ server.js
@@ -1,6 +1,21 @@
+const { json } = require('body-parser');
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+
+const mockData = [
+    {name: 'Promise'},
+    {name: 'Mark'}
+]
+
+app.get('/users', function(req, res){
+    return json({
+        success: true,
+        message: 'Retrieved users successfully',
+        users: mockData,
+    })
+})
 app.listen(PORT, function(){
     console.log('Listening on port' , PORT)
 })
